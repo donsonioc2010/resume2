e32e124776981d40f43b0b6e3e10c8fcfc1a48c2
donsonioc2010 <whddnjs822@gmail.com>
2022-05-15 01:53:02 +0900

    docs : etc수정

--- payload/presentation.ts
+++ payload/presentation.ts
@@ -5,17 +5,56 @@ const presentation: IPresentation.Payload = {
 
   list: [
     {
-      title: 'Lorem ipsum',
-      subTitle: 'consectetur adipiscing elit',
-      startedAt: '2017-10',
-      endedAt: '2022-10',
+      title: '디지털컨버전스 기반 Smart Web Content 개발자 양성과정',
+      subTitle: '비트캠프-강남점 (국비)',
+      startedAt: '2020-05',
+      endedAt: '2020-10',
       descriptions: [
         {
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          content: 'Java 및 Spring 교육',
         },
         {
-          content: 'Slideshare:',
-          postHref: 'https://www.slideshare.net//',
+          content: 'More..',
+          descriptions: [
+            {
+              content:
+                '두번의 프로젝트를 팀장을 진행하면서 PM으로써 일정관리에 대한 부분을 체험하였으며, 개발에 대한 선수 지식이 확실하지 않으면 확실한 일정을 산출하기 힘들다는 점을 체감함. ' +
+                '또한 혼자서는 모두 할 수 있긴 하나 한정된 시간안에서는 혼자서 모든 것을 전부 다 할 수는 없는 것을 알게되었던 시간',
+            },
+            {
+              content:
+                '인프라에 관심이 많음에도 불구하고 AWS를 타인과 같이 사용해볼 기회가 없어 해보지 못했었으나, 프로젝트를 진행해 보면서 실제로 적용할 수 있었음. ' +
+                '당시 서비스를 직접 해본다는 개념이 없었다 보니 아쉬운점으로 남음',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: '육군 전역',
+      subTitle: '병장만기전역 - 통신병',
+      startedAt: '2015-02',
+      endedAt: '2016-11',
+      descriptions: [
+        {
+          content: '고성 GOP 18개월 복무',
+        },
+        {
+          content: 'GOP 모든 소초의 PC상태, 네트워크, 무선통신 관리',
+        },
+      ],
+    },
+    {
+      title: '연세IT미래교육원 장안문캠퍼스',
+      subTitle: '네트워크 통신',
+      startedAt: '2013-03',
+      endedAt: '2013-11',
+      descriptions: [
+        {
+          content: '네트워크 IP통신 학습',
+        },
+        {
+          content: 'CISCO사의 네트워크 장비 셋팅 학습',
         },
       ],
     },
