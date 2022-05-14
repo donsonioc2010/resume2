2b280a535e264b247faf7e46a06d64f355711225
donsonioc2010 <whddnjs822@gmail.com>
2022-05-15 02:48:35 +0900

    docs : 자기소개 제작

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -5,10 +5,11 @@ const introduce: IIntroduce.Payload = {
   disable: false,
 
   contents: [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer massa magna, feugiat commodo arcu sed, sagittis sollicitudin tortor. Nullam diam ex, interdum nec lacus vel, ultrices pretium sapien. Proin bibendum non nibh eu aliquet. In sit amet arcu sed nisi tincidunt ultricies. Fusce magna tellus, egestas at pretium quis, rutrum et ante. Phasellus efficitur lectus felis, sed lacinia enim rhoncus ut. Sed id mi vitae neque aliquet porttitor. Fusce dignissim ipsum urna, at finibus lectus sodales ut. Nulla ut purus vestibulum, dapibus tellus consequat, porttitor purus. Nunc egestas tortor ut vehicula mollis.',
-    'Integer ut libero eu libero mattis rutrum at at urna. Integer non turpis ante. Suspendisse vestibulum odio id libero facilisis gravida. In ipsum est, pellentesque vestibulum tortor eu, bibendum imperdiet metus. Quisque ornare eget ex ut congue. Nam vel finibus sem. Phasellus id neque nibh. Nullam turpis velit, pharetra vitae mi non, bibendum maximus urna.',
+    '웹 서비스와 인프라에 많은 관심을 가지고 있는 개발자이며, Java, Oracle, AWS, Linux를 다뤄보며 개발, 유지보수 경험이 있습니다.',
+    '다른 사람들이 제작했던 사이트들의 유지보수를 진행하며 주석이 없거나 하드코딩되어있는 코드를 보며 많은 고생을 하다 보니, 누구나 이해를 하기 쉬운 코드를 작성하기 위해 많은 생각을 합니다.',
+    '기억력이 뛰어난 편이 아니다 보니 생활을 하며 자주 까먹는 경우가 많습니다. 이를 극복하기 위해서 기록이 가능한 대부분을 정리해서 기록하거나, 혹은 문서나 메뉴얼로 제작을 하기 위해 노력합니다.',
   ],
-  sign: 'Love poem',
+  sign: 'Jong1',
   // sign: packageJson.author.name,
   // latestUpdated: '2019-07-24',
   latestUpdated: lastestUpdatedAt,
